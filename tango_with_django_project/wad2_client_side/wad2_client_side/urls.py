a8de0b2a2ac41f5b01064d747c4ce374eb06d8c6
Calum Firth <2070853F@student.gla.ac.uk>
2019-01-31 17:36:06 +0000

    Finished end of chapter 4

--- tango_with_django_project/wad2_client_side/wad2_client_side/urls.py
+++ tango_with_django_project/wad2_client_side/wad2_client_side/urls.py
@@ -1,4 +1,4 @@
-"""tango_with_django_project URL Configuration
+"""wad2_client_side URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,15 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
-
-from rango import views
-
+from django.views.decorators.csrf import csrf_exempt
+from wad2 import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^rango/', include('rango.urls')),
-    url(r'^admin/', admin.site.urls),    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+url(r'^rangoTest/$', views.RangoTest, name='rango_test'),
+url(r'^testProgress/$', views.TestProgress, name='test_progress'),
+]   
