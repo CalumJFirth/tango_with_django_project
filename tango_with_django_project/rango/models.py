56c5e0bf55b54594fcf4fb38095b241d83bba6e9
Calum Firth <2070853F@student.gla.ac.uk>
2019-02-05 15:10:45 +0000

    Finished end of chapter 7

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -6,6 +6,8 @@ class Category(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+    
+    
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
